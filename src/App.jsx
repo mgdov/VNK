43d1c75fc67389af966d922eea84200281036f68
webddevel <unar0448@gmail.com>
2025-09-29 10:49:34 +0300

    обновление подвала на странице Admin

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from './components/Footer/Footer';
 import { AuthProvider } from './contexts/AuthContext';
 
 
-function App() {
+function AppContent() {
   const location = useLocation();
   const isAdminRoute = location.pathname.toLowerCase().startsWith('/admin');
   const routes = [
@@ -22,16 +22,24 @@ function App() {
 
   ]
   return (
+    <>
+      <div className="container max-w-[1200px] mx-auto px-[5px]">
+        <Routes>
+          {
+            routes.map((obj) => <Route key={obj.path} path={obj.path} element={obj.element} />)
+          }
+        </Routes>
+      </div>
+      {!isAdminRoute && <Footer />}
+    </>
+  )
+}
+
+function App() {
+  return (
     <BrowserRouter>
       <AuthProvider>
-        <div className="container max-w-[1200px] mx-auto px-[5px]">
-          <Routes>
-            {
-              routes.map((obj) => <Route key={obj.path} path={obj.path} element={obj.element} />)
-            }
-          </Routes>
-        </div>
-        {!isAdminRoute && <Footer />}
+        <AppContent />
       </AuthProvider>
     </BrowserRouter>
   )
