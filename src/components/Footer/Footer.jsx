57d60994b7eb51734de3d2e2776c7d0f26aae758
webddevel <unar0448@gmail.com>
2025-10-01 01:25:24 +0300

    отступ Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -31,7 +31,7 @@ function Footer() {
     };
 
     return (
-        <footer className="bg-gray-900 text-gray-100 w-full">
+        <footer className="bg-gray-900 mt-[30px] text-gray-100 w-full">
             <div className="mt-[80px] max-w-[1200px] mx-auto px-6 py-12 lg:py-16">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                     {/* Блок логотипа + описание */}
